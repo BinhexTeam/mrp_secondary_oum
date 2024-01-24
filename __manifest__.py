299fb4425e9e41a13365d056a180e737196ffba1
Adasat Torres de León <a.torres@binhex.cloud>
2024-01-24 11:53:25 +0000

    Add addon

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "espuma_custom",
+
+    'summary': """
+        Custom addon for espuma.""",
+
+
+    'author': "Binhex",
+    'website': "https://binhex.cloud",
+
+    'category': 'custom',
+    'version': '14.0.1.0.0',
+    'license': 'AGPL-3',
+    
+    'depends': ['base', 'mrp'],
+
+    'data': [
+        'views/mrp_production_views.xml',
+    ],
+}
