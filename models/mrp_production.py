40e6b6c6ef539f7ee11d7345d33c4b244a8e72c9
Adasat Torres de Leon <a.torres@binhex.cloud>
2024-02-23 08:40:54 +0000

    Add addon

--- models/mrp_production.py
+++ models/mrp_production.py
@@ -3,6 +3,7 @@ from odoo import fields, models, api
 class MRPProduction(models.Model):
     _inherit="mrp.production"
     
+    
     def action_refresh_secondary_unit_in_moves(self):
         for move in self.move_raw_ids:
             move.action_refresh_secondary_unit()
