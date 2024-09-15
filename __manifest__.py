e52fa2a01c9c887b41993ac53ae6a7e74cb875a0
r.perez <r.perez@binhex.cloud>
2024-09-15 00:35:58 -0400

    16.0-dev

--- __manifest__.py
+++ __manifest__.py
@@ -1,27 +1,28 @@
 {
-    'name': "Mrp Secondary Uom",
-
-    'summary': """
+    "name": "Mrp Secondary Uom",
+    "summary": """
         Show secondary unit on mrp.""",
-
-    'description': """
+    "description": """
         Show secondary unit on mrp.
     """,
-    'author': "Binhex",
-    'website': "https://binhex.cloud/",
-
+    "author": "Binhex",
+    "website": "https://binhex.cloud/",
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'summary': 'Manufacturing Orders & BOMs',
-    'version': "15.0.1.0.0",
-
+    "summary": "Manufacturing Orders & BOMs",
+    "version": "16.0.1.0.0",
     # any module necessary for this one to work correctly
-    'depends': ['base','mrp','product_secondary_unit','stock_secondary_unit','stock'],
-
+    "depends": [
+        "base",
+        "mrp",
+        "product_secondary_unit",
+        "stock_secondary_unit",
+        "stock",
+    ],
     # always loaded
-    'data': [
+    "data": [
         # 'security/ir.model.access.csv',
-        'views/mrp_production_views.xml',
+        "views/mrp_production_views.xml",
     ],
 }
