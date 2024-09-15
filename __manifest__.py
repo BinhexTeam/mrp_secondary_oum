8ee3fffb2b755513ca3e5942650f39da6fc141b9
r.perez <r.perez@binhex.cloud>
2024-09-15 00:39:29 -0400

    15.0-dev

--- __manifest__.py
+++ __manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 {
     'name': "Mrp Secondary Uom",
 
@@ -15,7 +14,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'summary': 'Manufacturing Orders & BOMs',
-    'version': '0.1',
+    'version': "15.0.1.0.0",
 
     # any module necessary for this one to work correctly
     'depends': ['base','mrp','product_secondary_unit','stock_secondary_unit','stock'],
