8ee0c09911208549a8c03f9b6d009faef250b28e
r.perez <r.perez@binhex.cloud>
2024-12-07 04:33:07 -0500

    [IMP] mrp_secondary_oum: improve manifest

--- __manifest__.py
+++ __manifest__.py
@@ -7,18 +7,13 @@
     """,
     "author": "Binhex",
     "website": "https://binhex.cloud/",
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    "summary": "Manufacturing Orders & BOMs",
     "version": "16.0.1.0.0",
+    "license": "AGPL-3",
     # any module necessary for this one to work correctly
     "depends": [
-        "base",
         "mrp",
         "product_secondary_unit",
         "stock_secondary_unit",
-        "stock",
     ],
     # always loaded
     "data": [
